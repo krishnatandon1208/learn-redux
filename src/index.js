b92d3264f0597040da306604c1f766fd67ddfce2
Krishna Tandon <krishnatandon.1208@gmail.com>
2020-11-28 03:58:17 +0530

    ACTION created

--- src/index.js
+++ src/index.js
@@ -3,16 +3,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from "redux";
+
+//let store = createStore(reducer);
 
 //STORE - GLOBALIZED STATE
+
+//ACTION
+const increment = () => {
+    return {
+        type: "INCREMENT" //this is the name of my action
+    }
+}
+
+//REDUCER
+
+//DISPATCH
 
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
